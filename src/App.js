a0b5d3006ffe819cc9d54982965b5a2bcd9d1854
rachel-os <rachel.otete@gmail.com>
2019-07-16 16:49:07 +0200

    layout structure

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React, { useState } from 'react';
-import { Menu, BackTop } from 'antd';
-import Logo from '../src/assets/images/star.png';
 import { Link } from 'react-router-dom';
+import { Layout, Menu } from 'antd';
+import Home from './Components/Home/Home'
+import Logo from '../src/assets/images/star.png';
 import './App.css';
 import "antd/dist/antd.css";
 
 function App() {
 
   const [toggleNavbar, setToggleNavbar] = useState('');
+  const { Header, Content, Footer } = Layout;
   const handleClick = e => {
     console.log('click ', e);
     setToggleNavbar({
@@ -17,39 +19,41 @@ function App() {
 
   return (
     <div className="App">
-      <nav>
-        <Link to='/'>
-          <div>
+      <Layout className="layout">
+        <Header>
+          <Link to='/'>
             <img className="logoWildCircus" alt="logo" src={Logo} title="Wild circus logo" />
-          </div>
-        </Link>
-        <Menu 
-          className="navigation"
-          onClick={handleClick} 
-          selectedKeys={[toggleNavbar]} 
-          mode="horizontal">
-          <Menu.Item key="events">
-            <Link to="/events">
-              Our events
-            </Link>
-          </Menu.Item>
-          <Menu.Item key="about">
-            <Link to="/about">
-              <a href="https://ant.design" target="_blank" rel="noopener noreferrer">
-                About us
-              </a>
-            </Link>
-          </Menu.Item>
-          <Menu.Item key="contact">
-            <Link to="/contact">
-              Contact
-            </Link>
-          </Menu.Item>
-        </Menu>
-      </nav>
-      <BackTop>
-        <div className="ant-back-top-inner">UP</div>
-      </BackTop>
+          </Link>
+          <Menu 
+            className="navigation"
+            onClick={handleClick}
+            selectedKeys={[toggleNavbar]}
+            theme="dark"
+            mode="horizontal">
+            <Menu.Item key="events">
+              <Link to="/events">
+                Our events
+              </Link>
+            </Menu.Item>
+            <Menu.Item key="about">
+              <Link to="/about">
+                <a href="https://ant.design" target="_blank" rel="noopener noreferrer">
+                  About us
+                </a>
+              </Link>
+            </Menu.Item>
+            <Menu.Item key="contact">
+              <Link to="/contact">
+                Contact
+              </Link>
+            </Menu.Item>
+          </Menu>
+        </Header>
+        <Content className="content">
+          <Home/>
+        </Content>
+      </Layout>
+      <Footer className="footer">Made with love @WildCodeSchool</Footer>
     </div>
   );
 }
