29b55f0d9a9cdb7d461ec1f6496433f1520c46a1
rachel-os <rachel.otete@gmail.com>
2019-07-16 16:24:45 +0200

    navbar ok

--- src/App.js
+++ src/App.js
@@ -1,11 +1,55 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Menu, BackTop } from 'antd';
+import Logo from '../src/assets/images/star.png';
+import { Link } from 'react-router-dom';
 import './App.css';
 import "antd/dist/antd.css";
 
 function App() {
+
+  const [toggleNavbar, setToggleNavbar] = useState('');
+  const handleClick = e => {
+    console.log('click ', e);
+    setToggleNavbar({
+      current: e.key,
+    });
+  };
+
   return (
     <div className="App">
-      <h1>Hello there!</h1>
+      <nav>
+        <Link to='/'>
+          <div>
+            <img className="logoWildCircus" alt="logo" src={Logo} title="Wild circus logo" />
+          </div>
+        </Link>
+        <Menu 
+          className="navigation"
+          onClick={handleClick} 
+          selectedKeys={[toggleNavbar]} 
+          mode="horizontal">
+          <Menu.Item key="events">
+            <Link to="/events">
+              Our events
+            </Link>
+          </Menu.Item>
+          <Menu.Item key="about">
+            <Link to="/about">
+              <a href="https://ant.design" target="_blank" rel="noopener noreferrer">
+                About us
+              </a>
+            </Link>
+          </Menu.Item>
+          <Menu.Item key="contact">
+            <Link to="/contact">
+              Contact
+            </Link>
+          </Menu.Item>
+        </Menu>
+      </nav>
+      <BackTop>
+        <div className="ant-back-top-inner">UP</div>
+      </BackTop>
     </div>
   );
 }
--- src/Components/Artists/Artists.jsx
+++ src/Components/Artists/Artists.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Artists() {
   return (
     <div>
-      
+      Meet the Artists
     </div>
   )
 }
