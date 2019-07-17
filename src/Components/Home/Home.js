9e6f9d2a138ed4f94af17cd850e3e51cb47b7351
rachel-os <rachel.otete@gmail.com>
2019-07-18 01:30:46 +0200

    post contactData 1/2"

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -10,7 +10,7 @@ export default function Home() {
       <Row type="flex" justify="center" className="hero" >
         <img className="hero-bg" src={circus} alt="hero background" />
       </Row>
-      <h1>Title</h1>
+      <h1>Wild Circus</h1>
       <Row type="flex" justify="center">
         <Col span={10}>
           <p>Pommy ipsum picalilly spend a penny eton mess sod's law made a pig's ear of it, baffled wibbly-wobbly timey-wimey stuff daft cow by 'eck love eton mess flip flops, tosser bangers and mash anorak gravy cheese and chips got a lot of brass. Bugger meat and two veg mush nowt chap accordingly mince pies a right corker, conked him one on the nose pezzy little doofer rambunctious Bob's your uncle.</p>
--- src/api/index.js
+++ src/api/index.js
@@ -1,8 +1,14 @@
 const express = require('express');
 const mysql = require('mysql');
 const api = express();
+const bodyParser = require('body-parser');
 
-api.use((req,res, next) => {
+api.use(bodyParser.json());
+api.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+api.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
   next();
 })
@@ -18,21 +24,34 @@ const connection = mysql.createConnection({
   password: "password"
 })
 
-api.get('/events', (req,res) => {
-  connection.query("SELECT * FROM event", (err,result) => {
-    if (err) throw err;
+api.get('/events', (req, res) => {
+  connection.query("SELECT name, image, price, date, location_id FROM event", (error,result) => {
+    if (error) throw error;
     res.send(result);
   });
 });
 
-connection.connect((err) => {
-  if (err) throw (err);
+api.post('/contact', (req,res)=> {
+  const contactData = req.body;
+  connection.query("INSERT INTO contact SET ?", contactData, (error, res) => {
+    if (error) {
+      console.log(error);
+      res.status(500).send("Erreur lors de la sauvegarde du contact.");
+    } else {
+      console.log(res);
+      res.sendStatus(200);
+    }
+  });
+});
+
+connection.connect((error) => {
+  if (error) throw (error);
   console.log("mySQL connected!")
 })
 
-api.listen(8000,(err)=> {
-  if(err) {
-    console.log(err);
+api.listen(8000,(error)=> {
+  if(error) {
+    console.log(error);
   } else {
     console.log("API running...");
   }
