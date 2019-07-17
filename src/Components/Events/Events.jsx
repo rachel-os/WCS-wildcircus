19486acaddb023ae43c1cc47b8ae1c63a12d827a
rachel-os <rachel.otete@gmail.com>
2019-07-17 23:05:48 +0200

    filter items

--- src/Components/Events/Events.jsx
+++ src/Components/Events/Events.jsx
@@ -1,12 +1,17 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import { Card, Icon, Row } from 'antd';
+import { Card, Icon, Row, Button } from 'antd';
 import './Events.css';
+import _ from 'underscore';
 
 export default function Events() {
 
   const { Meta } = Card;
   const [shows, setShows] = useState([]);
+  const [filterPrice, setFilterPrice] = useState(false);
+  const [filterName, setFilterName] = useState(false);
+  const [filterDate, setFilterDate] = useState(false);
+
 
   useEffect(() => {
     axios.get('http://localhost:8000/events')
@@ -15,31 +20,72 @@ export default function Events() {
       })
   }, [])
 
+  const filterShows = (tag, [setFunc, param]) => {
+    if(param) {
+      setShows(shows.reverse());
+    } else {
+      setShows(_.sortBy(shows, tag));
+    }
+    setFunc(!param)
+  }
+
   return (
-    <div className="showCards">
+    <div>
       <h1>Our Events</h1>
       <Row>
-      {shows.map((show, index) => {
-        return (
-          <Card
-            style={{ width: 300 }}
-            cover={
-              <img
-                alt="Wild Circus event"
-                src={show.image}
+        <Button
+          
+          type="default"
+          shape="round"
+          icon="caret-down"
+          onClick={() => filterShows('date', [setFilterDate, filterDate])}
+        >
+          Filter per date
+      </Button>
+        <Button
+          type="default"
+          shape="round"
+          icon="caret-down"
+          onClick={() => filterShows('name', [setFilterName, filterName])}
+        >
+          Filter per name
+      </Button>
+        <Button
+          type="default"
+          shape="round"
+          icon="caret-down"
+          onClick={() => filterShows('price', [setFilterPrice, filterPrice])}
+        >
+          Filter per price
+      </Button>
+      </Row>
+      <Row>
+        {shows.map((show, index) => {
+          return (
+            <div 
+            className="cardcontainer" 
+            key={index}
+            >
+            <Card
+              style={{ width: 300 }}
+              cover={
+                <img
+                  alt="Wild Circus event"
+                  src={show.image}
+                />
+              }
+              actions={[<Icon type="ellipsis" />]}
+              hoverable={true}
+              >
+              <Meta
+                title={<h3>{show.name}</h3>}
+                description={<p>{show.price}€</p>}
               />
-            }
-            actions={[<Icon type="ellipsis" />]}
-            hoverable= {true}
-          >
-            <Meta
-              title={show.name}
-              description={show.location, show.date}
-            />
-          </Card>
-        )
-      }
-      )}
+            </Card>
+            </div>
+          )
+        }
+        )}
       </Row>
     </div>
   )
