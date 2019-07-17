f8ce0ebe7b9ef0e517e8d9f71a68cceda7d9d906
rachel-os <rachel.otete@gmail.com>
2019-07-17 17:20:05 +0200

    contact form

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -1,9 +1,42 @@
 import React from 'react'
+import { Row, Col, Form, Input } from 'antd';
 
 export default function Contact() {
+
+  const formItemLayout = {
+    labelCol: {
+      xs: { span: 24 },
+      sm: { span: 5 },
+    },
+    wrapperCol: {
+      xs: { span: 24 },
+      sm: { span: 12 },
+    },
+  };
+
   return (
     <div>
-      Contact us
+      <h1>Contact us</h1>
+      <Col>
+        <Form {...formItemLayout}>
+          <Form.Item label="First name"
+            hasFeedback
+          >
+            <Input placeholder="Barnabé" />
+          </Form.Item>
+          <Form.Item label="Last name"
+            hasFeedback
+          >
+            <Input placeholder="Collins" />
+          </Form.Item>
+          <Form.Item
+            label="Email"
+            hasFeedback
+          >
+            <Input placeholder="barnabecollins@mail.com" />
+          </Form.Item>
+        </Form>
+      </Col>
     </div>
   )
 }
