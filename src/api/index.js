e7b769557b299b58dbe6aa3c588f30cae1e1469b
rachel-os <rachel.otete@gmail.com>
2019-07-18 13:23:28 +0200

    ; exit clear /q
    ;

--- src/api/index.js
+++ src/api/index.js
@@ -3,25 +3,29 @@ const mysql = require('mysql');
 const api = express();
 const bodyParser = require('body-parser');
 
+api.use(bodyParser.urlencoded({ extended: false }))
 api.use(bodyParser.json());
-api.use(bodyParser.urlencoded({
-  extended: true
-}));
-
-api.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  next();
-})
-
-api.get('/',(req,res) => {
-  res.send("Ok good");
-})
-
 const connection = mysql.createConnection({
   host: "localhost" ,
   user: "root",
   database: "wildcircus",
   password: "password"
+})
+
+connection.connect((error) => {
+  if (error) throw (error);
+  console.log("mySQL connected!")
+})
+
+api.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Origin, Content-Type, Accept');
+  next();
+})
+
+api.get('/',(req,res) => {
+  res.send("Ok good");
 })
 
 api.get('/events', (req, res) => {
@@ -33,21 +37,17 @@ api.get('/events', (req, res) => {
 
 api.post('/contact', (req,res)=> {
   const contactData = req.body;
-  connection.query("INSERT INTO contact SET ?", contactData, (error, res) => {
+  connection.query("INSERT INTO contact SET ?", contactData, (error, result) => {
     if (error) {
       console.log(error);
       res.status(500).send("Erreur lors de la sauvegarde du contact.");
     } else {
-      console.log(res);
       res.sendStatus(200);
     }
   });
 });
 
-connection.connect((error) => {
-  if (error) throw (error);
-  console.log("mySQL connected!")
-})
+
 
 api.listen(8000,(error)=> {
   if(error) {
